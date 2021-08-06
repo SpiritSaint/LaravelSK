600a3de5853d5f150097e11562a2dfcc32bf5536
Ian Torres <iantorres@outlook.com>
2021-08-06 19:28:18 -0400

    Coverage increased and test unnecesary removed.

--- tests/Feature/ProfileInformationTest.php
+++ tests/Feature/ProfileInformationTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
 use Laravel\Jetstream\Http\Livewire\UpdateProfileInformationForm;
 use Livewire\Livewire;
 use Tests\TestCase;
@@ -32,5 +34,18 @@ class ProfileInformationTest extends TestCase
 
         $this->assertEquals('Test Name', $user->fresh()->name);
         $this->assertEquals('test@example.com', $user->fresh()->email);
+    }
+
+    public function test_profile_information_can_be_updated_with_photo()
+    {
+        Storage::fake('avatars');
+
+        $this->actingAs($user = User::factory()->create());
+
+        $file = UploadedFile::fake()->image('avatar');
+
+        Livewire::test(UpdateProfileInformationForm::class)
+            ->set('photo', $file)
+            ->call('updateProfileInformation');
     }
 }
